8ca92ef024d5bc56c0b50b69014985fe28685cf5
Peter <lsomeday@yandex.ru>
2020-04-06 11:10:23 +0300

    добавил еще одну кнопку

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let h1 = document.querySelector('h1');
 let button = document.querySelector('.b-1');
+let button2 = document.querySelector('.b-2');
 
 
 button.onclick = () => {
     h1.style.backgroundColor = 'red';
+}
+
+button2.onclick = () => {
+    h1.style.backgroundColor = 'yellow';
 }
